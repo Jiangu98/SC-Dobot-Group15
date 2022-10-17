d124f3cd116a8881299c0aa956bfaacc5e8f4992
LJiang98 <113962666+Jiangu98@users.noreply.github.com>
2022-10-17 18:54:15 +1100

    working dobot initialisation

--- Other/Codes/dobotgrasping-main/working/ros_dobot.m
+++ Other/Codes/dobotgrasping-main/working/ros_dobot.m
@@ -7,7 +7,7 @@ rosinit;
 
 safetyStatusSubscriber = rossubscriber('/dobot_magician/safety_status');
 pause(2); %Allow some time for MATLAB to start the subscriber
-currentSafetyStatus = safetyStatusSubscriber.LatestMessage.Data
+currentSafetyStatus = safetyStatusSubscriber.LatestMessage%.Data
 
 %   Stfety codes
 %     INVALID - 0
@@ -35,7 +35,7 @@ send(safetyStatePublisher,safetyStateMsg);
 
 jointStateSubscriber = rossubscriber('/dobot_magician/joint_states'); % Create a ROS Subscriber to the topic joint_states
 pause(2); % Allow some time for a message to appear
-currentJointState = jointStateSubscriber.LatestMessage.Position % Get the latest message
+currentJointState = jointStateSubscriber.LatestMessage%.Position % Get the latest message
 
 %% Get current end effector pose
 
@@ -43,14 +43,13 @@ endEffectorPoseSubscriber = rossubscriber('/dobot_magician/end_effector_poses');
 pause(2); %Allow some time for MATLAB to start the subscriber
 currentEndEffectorPoseMsg = endEffectorPoseSubscriber.LatestMessage;
 % Extract the position of the end effector from the received message
-currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X,
-                              currentEndEffectorPoseMsg.Pose.Position.Y,
-                              currentEndEffectorPoseMsg.Pose.Position.Z]
+currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X, currentEndEffectorPoseMsg.Pose.Position.Y, currentEndEffectorPoseMsg.Pose.Position.Z]
+
 % Extract the orientation of the end effector
 currentEndEffectorQuat = [currentEndEffectorPoseMsg.Pose.Orientation.W,
-                          currentEndEffectorPoseMsg.Pose.Orientation.X,
-                          currentEndEffectorPoseMsg.Pose.Orientation.Y,
-                          currentEndEffectorPoseMsg.Pose.Orientation.Z];
+                          ... currentEndEffectorPoseMsg.Pose.Orientation.X,
+                          ... currentEndEffectorPoseMsg.Pose.Orientation.Y,
+                          ... currentEndEffectorPoseMsg.Pose.Orientation.Z];
 % Convert from quaternion to euler
 %[roll,pitch,yaw] = quat2eul(currentEndEffectorQuat);
 
